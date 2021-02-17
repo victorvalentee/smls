d7b1260b3ab1fd87ac8dbed5d337a83e776ca652
victorvalentee <victor.valente@gmail.com>
2021-02-17 12:18:05 -0300

    Added files to scaffolding and to the prepocessing package

--- model/train/run.py
+++
@@ -1,6 +0,0 @@
-def run():
-    print(f'Running machine leaning pipeline.')
-
-
-if __name__ == '__main__':
-    run()
---
+++ run_training.py
@@ -0,0 +1,25 @@
+def run():
+    print('Starting ML pipeline...')
+
+    # On-demand model building
+
+    # Data pipeline
+        # Ingestion
+        # Preprocessing
+
+    # Model Pipeline
+        # Train
+        # Test
+        # Evaluate
+        # Save model
+
+    # On-demand prediction
+        # Data Pipeline
+        # Predict
+        # Save predictions
+
+    print('ML pipeline finished.')
+
+
+if __name__ == '__main__':
+    run()
